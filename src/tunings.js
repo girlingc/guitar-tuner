f70637ae2ee349729072e6a47771bee135972e00
Christian Girling <girling.christian@gmail.com>
2022-04-05 23:45:39 -0700

    Remove frequencies from tunings

--- src/tunings.js
+++ src/tunings.js
@@ -1,24 +1,19 @@
 const guitarTunings = {
   fourString: {
-
+    "Open G (GDGB)": ["G2", "D3", "G3", "B3"],
+    "GCEA": ["G3", "C3", "E3", "A3"],
+    "Violin Standard (GDAE)": ["G2", "D3", "A3", "E4"],
+    "Open C (CGCE)": ["C3", "G3", "C4", "E4"],
+    "Open D (DADF#)": ["D3", "A3", "D4", "F4#"],
+    "Open A (AEAC#)": ["A2", "E2", "A3", "C4#"],
+    "Open E (EBEG#)": ["E2", "B2", "E3", "G3#"],
+    "Standard 4 String Bass (EADG)": ["E1", "A1", "D2", "G2"],
   },
-  fiveString: {
-
-  },
-  sixString: {
-
-  },
-  sevenString: {
-
-  },
-  eightString: {
-
-  },
-  nineString:{
-    
-  }
-
-}
-
+  fiveString: {},
+  sixString: {},
+  sevenString: {},
+  eightString: {},
+  nineString: {},
+};
 
 export default guitarTunings;
