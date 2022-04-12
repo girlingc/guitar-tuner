dce10da9041c9bb81057fb6d4a6531474c3fc2c0
Christian Girling <girling.christian@gmail.com>
2022-04-11 22:23:34 -0700

    Add buttons for different guitars

--- src/Components/Interface/index.jsx
+++ src/Components/Interface/index.jsx
@@ -4,6 +4,10 @@ import getTunings from "../../tunings";
 const Interface = () => {
   return (
     <div>
+      <button>Bass Guitar</button>
+      <button>Ukelele</button>
+      <button>Six String</button>
+      <button>Seven String Guitar</button>
       {getTunings("sixString", "E Standard (EADGBE)")}
     </div>
   )
